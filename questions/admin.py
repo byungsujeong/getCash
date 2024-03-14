5702e8fa00f7bbe13f24dcf76172af646826b25b
byungsu.jeong <byungsu.jeong88@gmail.com>
2024-03-15 00:11:07 +0900

    update admin file

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+
+    """ Question Admin Definition """
